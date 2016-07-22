14ab8a1d284f5e8fbf27e6e3f67c034904bdbc0f
Meta Studios Inc <theTeam@metastudiosinc.com>
2016-07-22 10:58:22 -0400

    refactor and error handling

--- src/cloudmanager.js
+++ src/cloudmanager.js
@@ -6,8 +6,6 @@
 //
 // Commands:
 //   cloud status - Checks GCE, DigitalOcean, Heroku and AWS for VM statuses
-//   heroku apps - fetches names of apps loaded to heroku
-//   heroku status - fetches the status of all apps loaded to heroku
 //
 // Notes:
 //   <optional notes required for the script>
@@ -18,66 +16,7 @@
 var spawn = require('child_process').spawn
 
 
-var getHerokuApps = function(callback) {
-
-  var build = spawn('heroku', ["apps"]);
-  build.stdout.on('data', function(data) {
-    var apps = [];
-    if (data) {
-      data = data.toString();
-      data = data.split("\n");
-      for (chunk of data) {
-        if (chunk.indexOf("===") < 0 && chunk.length > 0) {
-          apps.push(chunk)
-
-        }
-      }
-      callback(apps);
-    }
-
-
-  });
-
-}
-
-var getHerokuAppStatus = function(appname, callback) {
-  var build = spawn('heroku', ["ps", "-a", appname]);
-  build.stdout.on('data', function(data) {
-    callback(appname, data);
-  })
-
-}
-
-
-
 module.exports = function(robot) {
-  robot.hear(/heroku status/i, function(msg) {
-    getHerokuApps(function(apps) {
-      for (var i = 0; i < apps.length; i++) {
-
-        getHerokuAppStatus(apps[i], function(appName, data) {
-          var post = data.toString()
-          if (post.indexOf(appName) > -1 || post.indexOf("web") > -1) {
-            msg.send(post);
-          }
-
-
-        })
-      }
-
-    });
-
-  });
-
-  robot.hear(/heroku apps/i, function(msg) {
-    getHerokuApps(function(apps) {
-      for (var i = 0; i < apps.length; i++) {
-        msg.send(apps[i]);
-
-      }
-    });
-
-  });
 
   robot.hear(/cloud status/i, function(msg) {
     msg.send("I hit my head and forgot how to do that")
