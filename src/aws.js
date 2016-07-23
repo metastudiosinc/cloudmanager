97aceb456bbb6721d55413491b7009de4679cf62
Meta Studios Inc <theTeam@metastudiosinc.com>
2016-07-23 16:53:44 -0400

    AWS function and better responses

---
+++ src/aws.js
@@ -0,0 +1,50 @@
+// Description
+//   Hubot Script for managing cloud services
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   AWS status - fetches AWS info
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Meta Studios Inc <theTeam@metastudiosinc.com>
+
+spawn = require('child_process').spawn
+
+function getAWSstatus(callback) {
+  //aws ec2 describe-hosts
+  build = spawn('aws', ["ec2", "describe-hosts"])
+  build.stdout.on('data', function(data) {
+    var info = JSON.parse(data.toString())
+      callback(info.Hosts)
+  });
+  build.stderr.on('data', function(data) {
+    console.log(data.toString());
+    callback(data);
+  });
+
+}
+
+
+module.exports = function(robot) {
+
+  robot.hear(/(.*)AWS status(.*)/i, function(msg) {
+    getAWSstatus(function(data) {
+      console.log("aws call complete");
+      if(data.toString() != ""){
+        msg.send(data.toString())
+      }else{
+        msg.send("no active AWS hosts")
+      }
+
+    });
+
+  })
+
+
+
+}
--- src/digitalocean.js
+++ src/digitalocean.js
@@ -35,7 +35,7 @@ var getRandomName = function() {
   return ("action-jack");
 }
 
-var get
+
 
 var createDOServer = function(size, image, callback) {
   var name = getRandomName();
@@ -56,11 +56,11 @@ var createDOServer = function(size, image, callback) {
 var getDOdroplets = function(callback) {
   build = spawn('doctl', ["compute", "droplet", "list"])
   build.stdout.on('data', function(data) {
-    console.log(data);
+    console.log(data.toString());
     callback(data)
   });
   build.stderr.on('data', function(data) {
-    console.log(data);
+    console.log(data.toString());
     callback(data)
   });
 }
@@ -77,7 +77,12 @@ module.exports = function(robot) {
 
   robot.hear(/(.*)DO status(.*)/i, function(msg) {
     getDOdroplets(function(data) {
-      msg.send(data.toString())
+      if(data.toString() != "null"){
+        msg.send(data.toString())
+      }else{
+        msg.send("no active DigitalOcean hosts")
+      }
+
     });
 
   })
